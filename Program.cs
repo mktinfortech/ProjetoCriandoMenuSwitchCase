8db9bfe85f7e005b233a60ae1a0f023fbc06a0c7
mktinfortech <contato@mktinfortech.com.br>
2024-03-30 14:27:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+string? opcoes;
+
+while(true)
+{
+       
+    System.Console.WriteLine("\n =========================== Estruras Condicionais com Switch Case==================");
+    System.Console.WriteLine("\n\n         =========================== Criando Um Menu ==================");
+
+   
+
+    Console.Write("\n\n      1 = Adicionar");
+    Console.Write("     2 = Excluir");
+    Console.Write("     3 = Listar");
+    Console.WriteLine("     4 = Atualizar");
+
+    opcoes = Console.ReadLine();
+
+    
+         Console.Clear();
+
+   switch(opcoes)
+   {
+    
+        
+        case "1":
+        Console.WriteLine("Adicionando...");
+        break;
+         case "2":
+        Console.WriteLine("Excluindo...");
+        break;
+         case "3":
+        Console.WriteLine("Listando...");
+        break;
+         case "4":
+        Console.WriteLine("Atualizando...");
+        break;
+
+        default:
+        Console.WriteLine("Opção Invalida!");
+        break;
+     
+   }
+
+}
